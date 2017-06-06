e1f3126a7d1d697abd26d810805ee712b610bf66
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-06-06 10:33:42 +0100

    created product.model, app.component.ts and app.component.html

---
+++ src/app/product.model.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Josh on 05/06/2017.
+ */
+
+export class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number) {
+  }
+}
